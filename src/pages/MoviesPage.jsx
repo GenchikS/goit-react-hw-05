057396a568c26d8659f06c7d172c2f99f7ca75b4
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-09-14 23:30:21 +0300

    v.5.1.7

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -8,15 +8,22 @@ import { Link, useLocation, useSearchParams } from "react-router-dom";
 
 
 export default function MoviesPage() {
-  const [query, setQuery] = useState("")
-  const [searchQuery, setSearchQuery] = useState("");
   const [searchFilmArr, setSearchFilmArr] = useState([])
 
- //  ф-ція повернення значення form
+  //  створення параметру
+  const [params, setParams] = useSearchParams()
+
+  //  ф-ція повернення значення form
   const formSubmit = (evn) => {
+    //  створення параметру в url зі значенням query
+    params.set("query", evn.target.elements.query.value);
+    setParams(params);
+    console.log("evn", evn.target.elements.query.value);
+
     evn.preventDefault();
-    setQuery(searchQuery);
-    if (!searchQuery.trim()) {
+    // setQuery(evn.target.elements.query.value);
+    const search = evn.target.elements.query.value;
+    if (!search) {
       return iziToast.show({
         message: "Sorry, input field cannot be empty. Please try again!",
         messageColor: `rgb(0,0,0)`,
@@ -25,41 +32,41 @@ export default function MoviesPage() {
         progressBarColor: `rgb(255,0,0)`,
       });
     }
-    // перевірка значення парамутру
-    // console.log(`query`, evn.target.elements.query.value);
-    setSearchQuery("");
-    //  перевірка значення яке повертає form
-      // console.log("searchQuery", searchQuery);
+    evn.target.reset()
   };
-  
-  const onHandeleChange = (evn) => {
-    setSearchQuery(evn.target.value);
-  };
-  
+  const query = params.get(`query`) ?? "";
+
+  // const onHandeleChange = (evn) => {
+  //   setSearchQuery(evn.target.value);
+  // };
+
   useEffect(() => {
-    if (!query.trim()) {
-      return
+    if (!query) {
+      return;
     }
     const dataFeatch = async () => {
-        const { data } = await searchFilm(query);
-    //   console.log("data", data.results);
-        setSearchFilmArr(data.results);
+      const { data } = await searchFilm(query);
+      //   console.log("data", data.results);
+      setSearchFilmArr(data.results);
       // console.log("searchFilm", searchFilmArr);
     };
     dataFeatch();
   }, [query]);
   
+  // const params = useSearchParams()
+  
+
   const location = useLocation();
   console.log("MP", location)
   return (
     <div>
       <form onSubmit={formSubmit}>
         <input
-          value={searchQuery}
+          // value={query}
           name="query"
           type="text"
           placeholder="Input search"
-          onChange={onHandeleChange}
+          // onChange={onHandeleChange}
         />
         <button type="submit">Search</button>
       </form>
@@ -68,7 +75,7 @@ export default function MoviesPage() {
           searchFilmArr.length > 0 &&
           searchFilmArr.map((film) => (
             <li key={film.id}>
-              <Link to={`/movies/${film.id}`} state={location}>
+              <Link to={`/movies/${film.id}`} state={location} >
                 {film.title}
               </Link>
             </li>
